11ead7a6ca4a97c75e7458bdffe5642a0bd12803
GobiRajeshKumar <rajeshgobi46@gmail.com>
2022-09-27 17:51:27 +0530

    first Commit

--- Sources/MyNetworking/MyNetworking.swift
+++ Sources/MyNetworking/MyNetworking.swift
@@ -1,6 +1,24 @@
+import Foundation
+
 public struct MyNetworking {
-    public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func downloadData(fromURL url: URL, completionHandler: @escaping (_ data: Data?) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Error downloading data.")
+                completionHandler(nil)
+                return
+            }
+            
+            completionHandler(data)
+        }
+        .resume()
     }
 }
